a08dc7f57ba0229bae3888adff86dade55a99074
Tanhleno <tts.tanhleno@gmail.com>
2019-11-11 07:30:08 -0300

    Teste de ArqInstrucao

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "instrucao.h"
+#include "arq_instrucao.h"
 
 
 int main(){
     Instrucao ins;
-    int args[] = {255, 255, 255};
-    definirInstrucao(&ins, "cor", args, 3);
-    imprimirInstrucao(ins);
-    liberarInstrucao(&ins);
+    ArqInstrucao arq;
+    abrirArqInstrucao(&arq, "instrucao.txt");
+    while(lerArqInstrucao(arq, &ins)){
+        imprimirInstrucao(ins);
+        liberarInstrucao(&ins);
+    }
+    fecharArqInstrucao(&arq);
     return 0;
 }
