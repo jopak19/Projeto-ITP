70d24d3080009ea0467b49b40ccb9a13facc8049
Tanhleno <tts.tanhleno@gmail.com>
2019-11-11 03:25:35 -0300

    Teste atualizado

--- test_matriz.c
+++ test_matriz.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 
 
-typedef struct
-{
+typedef struct{
     int*** matriz;
     int largura;
     int altura;
 } Imagem;
 
-void imprimirImagem(Imagem* img);
+void imprimirImagem(Imagem img);
 void liberarImagem(Imagem* img);
-void alocarImagem(Imagem* img);
+Imagem alocarImagem(int altura, int largura);
 
-void alocarImagem(Imagem* img){
+Imagem alocarImagem(int altura, int largura){
+    Imagem img;
+    img.altura = altura;
+    img.largura = largura;
     printf("alocarImagem\n");
-    img->matriz = (int***) malloc(img->altura * sizeof(int**));
-    for (int i = 0; i < img->altura; i++){
-        img->matriz[i] = (int**) malloc(img->largura * sizeof(int*));
-        for (int j = 0; j < img->largura; j++){
-            img->matriz[i][j] = (int*) calloc(3, sizeof(int));
+    img.matriz = (int***) malloc(altura * sizeof(int**));
+    for (int i = 0; i < altura; i++){
+        img.matriz[i] = (int**) malloc(largura * sizeof(int*));
+        for (int j = 0; j < largura; j++){
+            img.matriz[i][j] = (int*) calloc(3, sizeof(int));
         }
     }
+    return img;
 }
 
 void liberarImagem(Imagem* img){
@@ -29,51 +32,29 @@ void liberarImagem(Imagem* img){
     for (int i = 0; i < img->altura; i++){
         for(int j = 0; j < img->largura; j++) {
             free(img->matriz[i][j]);
-            //printf("p[%d][%d] liberado!\n", i, j);
         }
         free(img->matriz[i]);
-        //printf("p[%d] liberado!\n", i);
     }
     free(img->matriz);
-    //printf("p liberado!\n");
 }
 
-void imprimirImagem(Imagem* img){
+void imprimirImagem(Imagem img){
     printf("imprimirImagem\n");
-    for (int i = 0; i < img->altura; i++){
-        for (int j = 0; j < img->largura; j++){
-            printf("(%d, %d, %d)\t", img->matriz[i][j][0], img->matriz[i][j][1], img->matriz[i][j][2]);
+    for (int i = 0; i < img.altura; i++){
+        for (int j = 0; j < img.largura; j++){
+            printf("(%d, %d, %d)\t", img.matriz[i][j][0], img.matriz[i][j][1], img.matriz[i][j][2]);
         }
         printf("\n");
     }
 }
 
+
 int main(){
     Imagem img;
-
-    scanf("%d %d", &img.altura, &img.largura);
-    alocarImagem(&img);
-    /*
-    img.matriz = (int***) malloc(2 * sizeof(int**));
-    for (int i = 0; i < 2; i++){
-        img.matriz[i] = (int**) malloc(2 * sizeof(int*));
-        for (int j = 0; j < 2; j++){
-            img.matriz[i][j] = (int*) calloc(3, sizeof(int));
-        }
-    }
-    */
-    imprimirImagem(&img);
+    int altura, largura;
+    scanf("%d %d", &altura, &largura);
+    img = alocarImagem(altura, largura);
+    imprimirImagem(img);
     liberarImagem(&img);
-    /*
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
-            free(img.matriz[i][j]);
-        }
-        free(img.matriz[i]);
-    }
-    free(img.matriz);
-    */
-
-    //printf("</main: fim>\n");
     return 0;
 }
